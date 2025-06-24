f89e6326af2df2f714127228be34ad199ed38b99
Kushal Mistry <kushal18.codes@gmail.com>
2025-06-24 16:00:03 +0530

    feat: integrate Tailwind CSS and refactor App component
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Removed existing CSS styles and replaced them with Tailwind CSS imports.
    - Simplified the App component to display a title using Tailwind CSS classes.
    - Deleted unused index.css file to clean up the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './vite.svg';
 import './app.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" rel="noopener" target="_blank">
-          {/** biome-ignore lint/performance/noImgElement: not using next as framework */}
-          <img alt="Vite logo" className="logo" src={viteLogo} />
-        </a>
-        <a href="https://react.dev" rel="noopener" target="_blank">
-          {/** biome-ignore lint/performance/noImgElement: not using next as framework */}
-          <img alt="React logo" className="logo react" src={reactLogo} />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => setCount((prevCount) => prevCount + 1)}
-          type="button"
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+      <h1 className="font-bold text-4xl">Mini Blog Viewer</h1>
+    </main>
   );
 }
 
