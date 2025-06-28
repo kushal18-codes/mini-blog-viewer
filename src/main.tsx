713949b5306616e7e718f1153825b32cb89cd814
Kushal Mistry <kushal18.codes@gmail.com>
2025-06-28 22:25:21 +0530

    fix(main): correct import path for App component
    - Changed import from './app' to './App'

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './app';
+import App from './App';
 import { queryClient } from './query-client';
 
 const rootElement = document.getElementById('root');
